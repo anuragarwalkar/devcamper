e596eb44684f411a68ff005099548095f520764f
Anurag Arwalkar <anuragarwalkar@gmail.com>
2019-12-24 14:07:33 +0530

    added validation

--- controller/bootcamp.js
+++ controller/bootcamp.js
@@ -23,13 +23,16 @@ exports.createBootcamp = async (req,res,next) =>{
         const bootcamp = await bootcampModel.create(req.body);
         res.status(200).json({success:true,data:bootcamp})
     } catch (error) {
-        next(error)
+        res.status(400).json({success:true,data:error})
     }
 };
 
 exports.updateBootcamp = async (req,res,next) =>{
     try {
-        const updatedBootcamp = await bootcampModel.findByIdAndUpdate(req.params.id,req.body, {new: true})
+        const updatedBootcamp = await bootcampModel.findByIdAndUpdate(req.params.id,req.body, {new: true,runValidators:true})
+        if(!updatedBootcamp){
+         return res.status(404).json({success:false,message:'Please send a valid id'})
+        }
         res.status(200).json({success:true,data:updatedBootcamp})
     } catch (error) {
         next(error);
